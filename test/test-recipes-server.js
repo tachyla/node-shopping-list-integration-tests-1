e1e7ab4a587d5fea4dae2b1ec3f1f3682a8d9a08
tachyla <tachylad.murray@gmail.com>
2017-04-21 12:05:58 -0700

    created test-recipes-server.js

---
+++ test/test-recipes-server.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('Recipes', function() {
+
+
+  before(function() {
+    return runServer();
+  });
+
+
+  after(function() {
+    return closeServer();
+  });
